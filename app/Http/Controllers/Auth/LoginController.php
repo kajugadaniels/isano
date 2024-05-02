f96578fb839b183cb972a6cdb91ef137da6bd43b
kajugadaniels <kajugadaniels@gmail.com>
2024-05-02 16:27:20 +0200

    Implement auth in controllers

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // $this->middleware('guest')->except('logout');
+
+        if (Auth::check() && Auth::user()->role_id == 1) {
+            $this->redirectTo = route("admin.dashboard");
+        } elseif (Auth::check() && Auth::user()->role_id == 2) {
+            $this->redirectTo = route("patient.dashboard");
+        } elseif (Auth::check() && Auth::user()->role_id == 3) {
+            $this->redirectTo = route("receptionist.dashboard");
+        }
+
+        $this->middleware("guest")->except("logout");
+    }
+}
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,9 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\PatientMiddleware;
+use App\Http\Middleware\ReceptionistMiddleware;
 
 class Kernel extends HttpKernel
 {
@@ -63,5 +66,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => AdminMiddleware::class,
+        'patient' => PatientMiddleware::class,
+        'receptionist' => ReceptionistMiddleware::class,
     ];
 }
