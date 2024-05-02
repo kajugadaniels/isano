21352cd6611137e99e07f9b82ca62f2282ae5acf
kajugadaniels <kajugadaniels@gmail.com>
2024-05-02 16:21:27 +0200

    Protect routes with middleware

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -22,8 +22,12 @@ class RedirectIfAuthenticated
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+            if (Auth::guard($guard)->check() && Auth::user()->role_id == 1) {
+                return redirect()->route("admin.dashboard");
+            } elseif (Auth::guard($guard)->check() && Auth::user()->role_id == 2) {
+                return redirect()->route("patient.dashboard");
+            } elseif (Auth::guard($guard)->check() && Auth::user()->role_id == 3) {
+                return redirect()->route('receptionist.dashboard');
             }
         }
 
